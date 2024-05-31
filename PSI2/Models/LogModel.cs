df73f79bad16205e2f4b68f7119ede8479a1c3fb
Cretu Cosmin <cretu.cosmin85@gmail.com>
2024-05-31 23:14:55 +0300

    adaugata clasa logger pentru jurnalul de operatiuni

---
+++ PSI2/Models/LogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSI2.Models
+{
+    public class LogModel
+    {
+        [Key]
+        public int LogID { get; set; }
+        public DateTime OperationDate { get; set; }
+        public string? Username { get; set; }
+        public string? OperationDescription { get; set; }
+    }
+}
---
+++ PSI2/Services/Logger.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSI2.Services
+{
+    public class Logger
+    {
+        private DbContextOptions<AppDbContext> _contextOptions;
+
+        public Logger()
+        {
+            InitializeDbContext();
+        }
+
+        private void InitializeDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = ConfigurationManager.ConnectionStrings["PSI"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+            _contextOptions = optionsBuilder.Options;
+        }
+
+        public async Task<bool> Log(LogModel log)
+        {
+            try
+            {
+                using (var context = new AppDbContext(_contextOptions))
+                {
+                    await context.Log.AddAsync(log);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Eroare la adaugarea unui log in db. {ex}");
+                return false;
+            }
+        }
+    }
+}
